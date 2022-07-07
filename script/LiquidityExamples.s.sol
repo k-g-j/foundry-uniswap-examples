17fe4d7320ef396f5eb44d7222a22890eb13e9bc
Kate Johnson <johnson.kat.g@gmail.com>
2022-07-07 17:56:30 -0400

    changed script name

--- script/LiquidityExamples.s.sol
+++ script/LiquidityExamples.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.7.6;
 import "forge-std/Script.sol";
 import {LiquidityExamples} from "src/LiquidityExamples.sol";
 
-contract SwapExamplesDeployScript is Script {
+contract LiquidityExamplesDeployScript is Script {
     function setUp() public {}
 
     function run() public {
